f42195edb3d3c275b9650aa7f2259ecc1e5268ec
Amirhamja422 <amirhamja422@gmail.com>
2023-08-12 23:59:08 +0600

    card design complited

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="h-screen flex items-center">
-    <!-- <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+<body class="h-screen flex items-center justify-center bg-gray-200">
+    <!-- <div class="p-8 max-w-sm mx-auto bg-white rounded-xl shadow-md">
         <div class="shrink-0">
             <img class="h-12 w-12" src="img/air-canada-plane.jpg" alt="ChitChat Logo">
         </div>
@@ -18,17 +18,19 @@
         </div>
     </div> -->
 
-    <div>
-       <img class="h-12 w-12" src="img/air-canada-plane.jpg" alt="ChitChat Logo">
+    <div class="max-w-sm mx-auto p-8 bg-white rounded-xl shadow-md space-y-2">
+       <img class="h-24 mx-auto rounded-full ring-4" src="img/air-canada-plane.jpg" alt="ChitChat Logo">
 
+    <div  class='text-center'>
         <div>
-            <div>
-                <P>Learn with amir</P>
-                <P>Amir YOutube Channel</P>
-            </div>
-            <button>Click Me</button>
+            <P class="text-lg text-black font-semibold">Learn with amir</P>
+            <P class="text-gray-500 font-medium">Amir YOutube Channel</P>
         </div>
+        <button class="px-4 py-1 border border-purple-200 rounded-full text-sm text-purple-600 font-semibold">Click Me</button>
     </div>
+    </div>
+
+
 </body>
 
 </html>
