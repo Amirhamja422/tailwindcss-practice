b088d0e6b7fc5cda367bdb50e2a9ee4fd1ea95a3
Amirhamja422 <amirhamja422@gmail.com>
2023-08-27 13:12:16 +0600

    hover

--- index.php
+++ index.php
@@ -18,15 +18,15 @@
         </div>
     </div> -->
 
-    <div class="max-w-sm mx-auto p-8 bg-white rounded-xl shadow-md space-y-2">
-       <img class="h-24 mx-auto rounded-full ring-4" src="img/air-canada-plane.jpg" alt="ChitChat Logo">
+    <div class="max-w-sm mx-auto p-8 bg-white rounded-xl shadow-md space-y-2 sm:text sm:items-center sm:py-4 sm:space-y-0 sm:space-x-6">
+       <img class="h-24 mx-auto rounded-full ring-4 sm:flex-shrink-0 transform hover:scale-105 duration-500" src="img/air-canada-plane.jpg" alt="ChitChat Logo">
 
     <div  class='text-center'>
         <div>
             <P class="text-lg text-black font-semibold">Learn with amir</P>
             <P class="text-gray-500 font-medium">Amir YOutube Channel</P>
         </div>
-        <button class="px-4 py-1 border border-purple-200 rounded-full text-sm text-purple-600 font-semibold">Click Me</button>
+        <button class="px-4 py-1 border border-purple-200 rounded-full text-sm text-purple-600 font-semibold hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Click Me</button>
     </div>
     </div>
 
